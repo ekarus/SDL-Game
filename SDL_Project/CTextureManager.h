fc646c34624c77104ecd1a3b2c8ddb399ebdfb27
ekarus <ekarus303@gmail.com>
2013-10-15 08:47:01 +0300

    -

--- SDL_Project/CTextureManager.h
+++ SDL_Project/CTextureManager.h
@@ -6,7 +6,7 @@
 class CTextureManager
 {
 public:
-	//возвращает указатель на экзмепл€р синглтона
+	//возвращает указатель на экземпл€р синглтона
 	static CTextureManager* getInst()
 	{
 		static CTextureManager inst;
@@ -14,21 +14,31 @@ public:
 	}
 	//загружает текстуру из файла и возвращает ид, 0 - если ошибка
 	Uint32 LoadTexture(std::string file);
-	//удал€ет указаную текстуру
+	//удал€ет указанную текстуру
 	void DeleteTexture(Uint32 id);
 	//рисует текстуру в точке х,у с шириной w, высотой h и углом поворота angle
 	void DrawTexture(Uint32 id,int x,int y,int w,int h, int angle=0);
 	//рисует текстуру часть текстуры (x2,y2,w2,h2) в точке х,у с шириной w, высотой h и углом поворота angle
 	void DrawTexture(Uint32 id,int x,int y,int w,int h, int x2,int y2, int w2,int h2 ,int angle=0);
-	//устанавливает цвет дл€ указаной текстуры в формате RGBA
+	//устанавливает цвет дл€ указанной текстуры в формате RGBA
 	void SetTextureColor(Uint8 r,Uint8 g, Uint8 b, Uint8 a);
-	//возвращаеь ширину текстуры
+	//возвращает ширину текстуры
 	int getTextureWidth(Uint32 id);
-	//возвращаеь высоту текстуры
+	//возвращает высоту текстуры
 	int getTextureHeight(Uint32 id);
 	//удал€ет все текстуры
 	void DeleteAllTexture();
 
 private:
-	std::map< Uint32, SDL_Texture > m_Textures;
+
+	class Texture
+	{
+	public:
+		int w;
+		int h;
+		SDL_Texture* tex;
+		std::string file;
+	};
+
+	std::map< Uint32, Texture > m_Textures;
 };
